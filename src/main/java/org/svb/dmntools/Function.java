103ef423bef9f4028af05d4905c8d6cd6396d0d4
langph <p_de_lang@hotmail.com>
2019-01-22 15:53:27 +0100

    Mix of Regex and function name replace, Java does not support recursive Regex

--- src/main/java/org/svb/dmntools/Function.java
+++ src/main/java/org/svb/dmntools/Function.java
@@ -6,11 +6,44 @@ public class Function {
 
     private String feelFunction;
     private String opaFunction;
+    private Boolean regex;
+
+    public Boolean getRegex() {
+        return regex;
+    }
+
+    public void setRegex(Boolean regex) {
+        this.regex = regex;
+    }
+
 
     public String getFeelFunction() {
         return feelFunction;
     }
 
+    public String getOpaFunctionName(){
+
+        String name = "";
+        int index = this.opaFunction.indexOf("(");
+
+        if (index > 0) {
+            name = this.opaFunction.substring(0, index);
+        }
+
+        return name;
+    }
+
+    public String getFeelFunctionName(){
+
+        String name = "";
+        int index = this.feelFunction.indexOf("\\");
+
+        if (index > 0) {
+            name = this.feelFunction.substring(0, index);
+        }
+
+        return name;
+    }
 
     @XmlElement(name = "feel")
     public void setFeelFunction(String feelFunction) {
--- src/main/java/org/svb/dmntools/FunctionTranslator.java
+++ src/main/java/org/svb/dmntools/FunctionTranslator.java
@@ -3,12 +3,11 @@ package org.svb.dmntools;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 @XmlRootElement(name = "functions")
 public class FunctionTranslator {
@@ -33,108 +32,123 @@ public class FunctionTranslator {
 
     public void createFunctionInstances(){
 
-        Function fu = new Function();
+        Function fu;
 
-        fu.setFeelFunction("contains\\((\".*\"),(\".*\")\\)");
+        fu = new Function();
+        fu.setFeelFunction("contains\\(([^;,\\n\\r]*?),([^;,\\n\\r]*?)\\)");
         fu.setOpaFunction("Contains($1;$2)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("lower case\\((\".*\")\\)");
+        fu.setFeelFunction("lower case\\(([^;,\\n\\r]*?)\\)");
         fu.setOpaFunction("Lower($1)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("upper case\\((\".*\")\\)");
+        //fu.setFeelFunction("upper case\\(((?:.*|(?R))?)\\)"); recursive, not supported bij Java
+        fu.setFeelFunction("upper case\\(([^;,\\n\\r]*?)\\)");
         fu.setOpaFunction("Upper($1)");
+        fu.setRegex(false);
         this.add(fu);
 
 
         fu = new Function();
-        fu.setFeelFunction("starts with\\((\".*\"),(\".*\")\\)");
+        fu.setFeelFunction("starts with\\((.*?),(.*?)\\)");
         fu.setOpaFunction("StartsWith($1;$2)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("ends with\\((\".*\"),(\".*\")\\)");
+        fu.setFeelFunction("ends with\\((.*?),(.*?)\\)");
         fu.setOpaFunction("EndsWith($1;$2)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("string\\((\".*\")\\)");
+        fu.setFeelFunction("string\\((.*?)\\)");
         fu.setOpaFunction("Text($1)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("string length\\((\".*\")\\)");
+        fu.setFeelFunction("string length\\((.*?)\\)");
         fu.setOpaFunction("Length($1)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
-        fu.setFeelFunction("substring\\((\".*\"),(\\d*),(\\d*)\\)");
+        fu.setFeelFunction("substring\\((.*?),(\\d*?),(\\d*?)\\)");
         fu.setOpaFunction("Substring($1;$2;$3)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
         fu.setFeelFunction("date\\((\"[12]\\d{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01])\")\\)");
         fu.setOpaFunction("Date($1)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
         fu.setFeelFunction("date\\(([12]\\d{3}),(\\d{2}),(\\d{2})\\)");
         fu.setOpaFunction("Date($1;$2;$3)");
+        fu.setRegex(false);
         this.add(fu);
 
         fu = new Function();
         fu.setFeelFunction("([12]\\d{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01]))\\s?([+-])\\s?[P](\\d\\d?)[D]");
-        fu.setOpaFunction("AddDays($1;$2)");
+        fu.setOpaFunction("AddDays($1;$2$3)");
+        fu.setRegex(true);
         this.add(fu);
 
         fu = new Function();
         fu.setFeelFunction("([12]\\d{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01]))\\s?([+-])\\s?[P](\\d\\d?)[M]");
-        fu.setOpaFunction("AddMonths($1;$2)");
+        fu.setOpaFunction("AddMonths($1;$2$3)");
+        fu.setRegex(true);
         this.add(fu);
 
         fu = new Function();
         fu.setFeelFunction("([12]\\d{3}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12]\\d|3[01]))\\s?([+-])\\s?[P](\\d\\d?)[Y]");
-        fu.setOpaFunction("AddYears($1;$2)");
+        fu.setOpaFunction("AddYears($1;$2$3)");
+        fu.setRegex(true);
         this.add(fu);
 
     }
 
-    // not implemented because of escape problems in xml
-    public static FunctionTranslator readFunctionFile(String folder){
+    public String transformFunctions(String inputDMN){
 
-        try {
-
-            File file = new File("./src/main/functions.xml");
-            //File file = new File( folder + "/functions.xml");
-            JAXBContext jaxbContext = JAXBContext.newInstance(FunctionTranslator.class);
-
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-           // ft = (FunctionTranslator) jaxbUnmarshaller.unmarshal(file);
-
-           // System.out.println(ft);
-
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    String transformFunctions(String inputDMN){
+        // Java does not support recursive regex expressions, and therefore does not always return
+        // the correct result with functions inside functions.
+        // So until this changes or another solution is found, we only use it if needed: If the transformation is
+        // more difficult then just changing the name of the function.
+        // Really ugly solution, but no choice for now.
 
         String transformedInput = inputDMN;
 
         for (Function fu: this.functions){
 
-            if (transformedInput.matches(fu.getFeelFunction())) {
-               transformedInput =  transformedInput.replaceAll(fu.getFeelFunction(), fu.getOpaFunction());
+            if (fu.getRegex()) {
+                transformedInput = this.regexTransform(transformedInput, fu);
+            } else {
+                transformedInput = transformedInput.replace(fu.getFeelFunctionName(),fu.getOpaFunctionName());
             }
         }
-
         transformedInput = transformedInput.replace(".", "," );
         return  transformedInput;
     }
 
+
+    private String regexTransform(String inputDMN, Function fu){
+        String transformedInput = inputDMN;
+
+        Pattern p = Pattern.compile(fu.getFeelFunction());
+        Matcher m = p.matcher(transformedInput);
+        if (m.find()){
+            transformedInput =  transformedInput.replaceAll(fu.getFeelFunction(), fu.getOpaFunction());
+            System.out.println("Gevonden " + fu.getFeelFunction() + "Input: " + inputDMN);
+        }
+
+        return transformedInput;
+    }
 }
